e0e227a754bab86034270761d7153f4d90b74352
doughznutz <doughznutz@users.noreply.github.com>
2025-07-16 07:25:46 +0000

    Automated update from container

--- cloud/entrypoint.sh
+++ cloud/entrypoint.sh
@@ -1,4 +1,43 @@
 #!/bin/bash
 set -e
 
-echo "Get your head out of the clouds"
+# Source the functions
+source /app/functions.sh
+
+# Main command dispatcher
+COMMAND=$1
+
+if [ -z "$COMMAND" ]; then
+    echo "Usage: cloud <command> [options]"
+    echo "Commands:"
+    echo "  create <instance-name> - Create a new instance"
+    echo "  start <instance-name>  - Start an instance"
+    echo "  stop <instance-name>   - Stop an instance"
+    echo "  delete <instance-name> - Delete an instance"
+    echo "  list                   - List all instances"
+    exit 1
+fi
+
+shift # Remove command from argument list
+
+case "$COMMAND" in
+    create)
+        create_instance "$@"
+        ;;
+    start)
+        start_instance "$@"
+        ;;
+    stop)
+        stop_instance "$@"
+        ;;
+    delete)
+        delete_instance "$@"
+        ;;
+    list)
+        list_instances "$@"
+        ;;
+    *)
+        echo "Unknown command: $COMMAND"
+        exit 1
+        ;;
+esac
